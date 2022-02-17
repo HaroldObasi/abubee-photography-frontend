c4a62599c84c53ce6698a3336704371404ccd4c6
Harold Obasi <haroldobasi2k16@gmail.com>
2022-02-17 23:45:59 +0100

    Used table to render messages

--- src/screens/Messages.js
+++ src/screens/Messages.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllMessages } from "../store/actions";
-import MsgItem from "../components/MsgItem";
+import Table from "react-bootstrap/Table";
 
 const Messages = () => {
 	const messages = useSelector((state) => state.messages.messages);
+	var count = 0;
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -13,7 +14,48 @@ const Messages = () => {
 	return (
 		<>
 			<div className="message-title">INBOX</div>
-			{messages ? (
+			<Table
+				striped
+				bordered
+				hover
+				variant="dark"
+				className="msg-table"
+				responsive="xl"
+			>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th>Message</th>
+					</tr>
+				</thead>
+				<tbody>
+					{messages ? (
+						messages.map((item) => {
+							count++;
+							return (
+								// <MsgItem
+								// 	className="border-item"
+								// 	body={item.body}
+								// 	senderName={item.senderName}
+								// 	read={item.read}
+								// 	senderNum={item.senderNum}
+								// 	senderEmail={item.senderEmail}
+								// />
+								<tr>
+									<td>{count}</td>
+									<td>{item.senderName}</td>
+									<td>{item.body}</td>
+								</tr>
+							);
+						})
+					) : (
+						<> </>
+					)}
+				</tbody>
+			</Table>
+
+			{/* {messages ? (
 				messages.map((item) => (
 					<MsgItem
 						className="border-item"
@@ -26,7 +68,7 @@ const Messages = () => {
 				))
 			) : (
 				<> </>
-			)}
+			)} */}
 		</>
 	);
 };
