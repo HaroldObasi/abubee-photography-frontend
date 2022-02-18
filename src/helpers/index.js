bd7acb11dd4514e21da3dcfdb86f6ef46b73f0a2
Harold Obasi <haroldobasi2k16@gmail.com>
2022-02-18 14:28:16 +0100

    Added signout helper function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,13 @@
 export const imgUrlTransform = (width, height, keyword, urlArr) => {
-    urlArr = urlArr.split('/')
-    width = String(width)
-    height = String(height)
-    let posIndex = urlArr.indexOf(keyword)
-    urlArr.splice(posIndex + 1 , 0, `w_${width}/h_${height}`)
-    return urlArr.join('/')
-}
+	urlArr = urlArr.split("/");
+	width = String(width);
+	height = String(height);
+	let posIndex = urlArr.indexOf(keyword);
+	urlArr.splice(posIndex + 1, 0, `w_${width}/h_${height}`);
+	return urlArr.join("/");
+};
+
+export const signout = () => {
+	localStorage.removeItem("auth-token");
+	window.location.reload(false);
+};
