f3187f4268ce3d7bec34bbf26d6ac4cf0154e00d
Harold Obasi <haroldobasi2k16@gmail.com>
2022-02-18 14:23:36 +0100

    Refactored Code for Logo and minor styling changes

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -1,29 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Logo from "../Logo";
 const Menu = () => {
 	return (
 		<>
 			<div className="menu py-4 px-5 ">
 				<Link to="/">
-					<div className="main-logo">
-						<div className="logo-top">Abubakar Ali</div>
-						<div className="text">
-							PHOTOGRAPHER <span className="ampersand">&</span> CREATIVE
-							DIRECTOR
-						</div>
-					</div>
+					<Logo />
 				</Link>
 
 				<div className="menu-options">
-					<Link className="text-hover-1" to="/">
-						works
-					</Link>
 					<Link className="text-hover-1" to="/hire">
-						hire
+						Hire
 					</Link>
 					<Link className="text-hover-1" to="/about">
-						about
+						About
+					</Link>
+					<Link className="text-hover-1" to="/signin">
+						Sign-in
 					</Link>
 				</div>
 			</div>
