159fc24ac444180f28e9e719a5aa26e3fec3964e
Harold Obasi <haroldobasi2k16@gmail.com>
2022-02-18 14:21:45 +0100

    Added Logo Component to contain Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+const Logo = () => {
+	return (
+		<>
+			<div className="main-logo">
+				<div className="logo-top">Abubakar Ali</div>
+				<div className="text">
+					PHOTOGRAPHER <span className="ampersand">&</span> CREATIVE DIRECTOR
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Logo;
