ae242f7123bf70d77c03c1e53dd7082c02402cb5
Harold Obasi <haroldobasi2k16@gmail.com>
2022-02-18 14:28:34 +0100

    General style changes

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,92 +1,98 @@
-import React, { useState, useEffect } from "react"
-import { Spinner } from "react-bootstrap"
-import { signIn } from "../store/actions"
-import { useHistory } from "react-router"
-import { useSelector, useDispatch } from "react-redux"
+import React, { useState, useEffect } from "react";
+import { Spinner, Button } from "react-bootstrap";
+import { signIn } from "../store/actions";
+import { useHistory } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
 
 const Signin = () => {
-    const history = useHistory()
-    const dispatch = useDispatch()
-    const [loading, setLoading] = useState(false)
-    const [login, setLogin] = useState({
-        email: '',
-        password: ''
-    })
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const [loading, setLoading] = useState(false);
+	const [login, setLogin] = useState({
+		email: "",
+		password: "",
+	});
 
-    const token = useSelector( state => state.admin.token )
+	const token = useSelector((state) => state.admin.token);
 
-    useEffect(() => {
-        if(token){
-            history.push('/')
-        }
-    }, [token])
+	useEffect(() => {
+		if (token) {
+			history.push("/");
+		}
+	}, [token]);
 
-    const handleInputChange = (event) => {
-        event.preventDefault();
-        setLogin({
-            ...login, 
-            [event.target.name] : event.target.value 
-        })
-    }
+	const handleInputChange = (event) => {
+		event.preventDefault();
+		setLogin({
+			...login,
+			[event.target.name]: event.target.value,
+		});
+	};
 
-    const handleSignin = (event) => {
-        event.preventDefault()
-        setLoading(true)
-        dispatch( signIn(login) ).then(
-            setLoading(false)
-        )
+	const handleSignin = (event) => {
+		event.preventDefault();
+		setLoading(true);
+		dispatch(signIn(login)).then(setLoading(false));
+	};
 
-    }
+	return (
+		<>
+			<div className="signup-body">
+				<div>
+					<div className="signup-title mb-3 text-center">Log-In</div>
 
-    return(
-        <>
-            <div className = "signup-body">
-                <div>
-                    <div className = "signup-title mb-3">
-                        Site Admin Log-In
-                    </div>
+					<form action="" onSubmit={handleSignin}>
+						<div className="signup-fields">
+							<div className="field">
+								<div className="for-label">
+									<label htmlFor="emailInput">EMAIL:</label>
+								</div>
+								<div className="field-input">
+									<input
+										name="email"
+										type="text"
+										onChange={handleInputChange}
+									/>
+								</div>
+							</div>
+							<div className="field">
+								<div className="for-label">
+									<label htmlFor="emailInput">PASSWORD:</label>
+								</div>
+								<div className="field-input">
+									<input
+										name="password"
+										type="password"
+										onChange={handleInputChange}
+									/>
+								</div>
+							</div>
+						</div>
+						{/* <button className="signup-btn mt-4" type="submit">
+							submit.
+							{loading ? (
+								<span>
+									<Spinner animation="border" variant="light" size="sm" />
+								</span>
+							) : null}
+						</button> */}
+						<br />
+						<div className="text-center">
+							<Button
+								variant="outline-light"
+								type="submit"
+								className="text-center button"
+							>
+								<span className="btn-text">Sign In</span>
+							</Button>
+						</div>
+					</form>
 
-                    <form action="" onSubmit = {handleSignin}>
-                        <div className = "signup-fields">
-                            <div className = 'field'>
-                                <div className = "for-label">
-                                    <label htmlFor = "emailInput">EMAIL {">>>"}</label>
-                                </div>
-                                <div className = "field-input">
-                                    <input name = "email" type="text" onChange = { handleInputChange }/>
-                                </div>
-                            </div>
-                            <div className = 'field'>
+					<br />
+				</div>
+			</div>
+		</>
+	);
+};
 
-                                <div className = "for-label">
-                                    <label htmlFor = "emailInput">PASSWORD {">>>"}</label>
-                                </div>
-                                <div className = "field-input">
-                                    <input name = "password" type="password" onChange = { handleInputChange }/>
-                                </div>
-                            </div>
-                        </div>
-                        <button 
-                            className = "signup-btn mt-4"
-                            type = "submit"
-                        >
-                            submit.
-                            {
-                            loading ? 
-                                <span>
-                                <Spinner animation="border" variant="light" size = "sm"/>
-                                </span>
-                                :
-                                null
-                            }
-                        </button>
-                    </form>
-
-                    <br />
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default Signin
+export default Signin;
